d36d030165591a3a77f5a20231e3fe066670a94d
Hoàng Thanh Hà <49011681+hoangha090399@users.noreply.github.com>
2020-04-23 09:56:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title>Cloud-Php-Ex000</title>
+  <title>Bai tap</title>
   <meta charset="utf-8">
 </head>
 <body>
   <h1>Triangle</h1>
   <p>Hello my name is Ha</p>
-  <form action=”action.php” method=”get”>
-   Do dai 1: <input type=”text” name=”a”><br/>
-   Do dai 2: <input type=”text” name=”b”><br/>
-   Do dai 3: <input type=”text” name=”c”><br/>
-   <input type=”submit” value=”Tính toán”>
-</form>
+  <form action=â€action.phpâ€ method=â€getâ€>
+   Do dai 1: <input type=â€textâ€ name=â€aâ€><br/>
+   Do dai 2: <input type=â€textâ€ name=â€bâ€><br/>
+   Do dai 3: <input type=â€textâ€ name=â€câ€><br/>
+   <input type=â€submitâ€ value=â€doâ€>
+  </form>
 </body>
 </html>
